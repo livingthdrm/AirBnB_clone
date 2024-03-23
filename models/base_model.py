988ff2fbb28b7ce1d9c6a53ef12466c7eeccd821
livingthdrm <salaonjoseph@gmail.com>
2024-03-23 13:27:10 -0700

    BaseModel class update

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
-"""a class BaseModel that defines all common attributes/methods for other classe"""
+import uuid
+import datetime
+"""a class BaseModel that defines
+all common attributes/methods for other classes"""
 
 
 class BaseModel:
     """This is the base class of the entire application
 
     name: BaseModel"""
-    id = uuid.uuid4()
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    id = str(uuid.uuid4())
+    created_at = datetime.datetime.now()
+    updated_at = datetime.datetime.now()
     
     def __str__(self):
         """ A module that prints
         [<class name>] (<self.id>) <self.__dict__> """
-
-        print(self.name)
-        print(self.id)
-        print(self.__dict__)
+        class_name = type(self).__name__
+        return f"[{class_name}] ({self.id}) <{self.__dict__}>"
     
     def save(self):
         """ A method to update public instance attribute
         updated_at with the current datetime """
 
-        BaseModel.updated_at = datetime.now()
+        BaseModel.updated_at = datetime.datetime.now()
+        print("This model was updated at: {}".format(BaseModel.updated_at))
 
     def to_dict(self):
         """ returns a dictionary containing all
         keys/values of __dict__ of the instance """
+        
+        #self.__dict__ = {'self.__class__' = 'self.name')
+        for key, value in self.__dict__:
+            print(key, value)
 
-        self.__dict__ = self
-
+print("Does my code work?")
+print()
+the_model = BaseModel()
+print()
+print(the_model)
+print()
+print(str(the_model))
+the_model.to_dict()
+print()
+the_model.save()
